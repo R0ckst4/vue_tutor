dc8461de99bcdacfc8add80a54ebd94bc64bbf1f
R0ck5t4 <actuallyamarocksta@gmail.com>
2022-11-01 22:33:47 +0300

    Worked with mixins, add some simple hideDialog mixin

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -1,49 +1,34 @@
 <template>
-    <div class ="dialog" v-if="show" @click="hideDialog">
-        <div @click.stop class="dialog__content">
-            <slot></slot>
-        </div>
-        
+  <div class="dialog" v-if="show" @click="hideDialog">
+    <div @click.stop class="dialog__content">
+      <slot></slot>
     </div>
+  </div>
 </template>
 <script>
+import toggleMixin from '@/mixins/toggleMixin';
+
 export default {
-    name: "my-dialog",
-    props: {
-        show:{
-            type: Boolean,
-            default : false,
-
-        }
-    },
-    methods: {
-        hideDialog(){
-            this.$emit('update:show', false);
-
-        }
-    },
-    
-}
+  name: 'my-dialog',
+  mixins: [toggleMixin],
+};
 </script>
 <style>
 .dialog {
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-    background: rgba(0,0,0, 0.5);
-    position: fixed;
-    display: flex;
-
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
 }
 .dialog__content {
-    margin: auto;
-    background: #fff;
-    border-radius: 12px;
-    min-height: 50px;
-    min-width: 300px;
-    padding: 20px;
-
+  margin: auto;
+  background: #fff;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
 }
-    
 </style>
